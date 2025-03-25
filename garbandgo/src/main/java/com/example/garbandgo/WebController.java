d94854edfa4716127640eb38ffc6527857a6cfcc
Mila Yonkova <48622362+MilaDimitrova@users.noreply.github.com>
2025-03-25 13:34:49 +0200

    spring boot/ html and css files

---
+++ garbandgo/src/main/java/com/example/garbandgo/WebController.java
@@ -0,0 +1,13 @@
+package com.example.garbandgo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public String homePage() {
+        System.out.println("Method homePage called");
+        return "homePage"; // Трябва да съвпада с името на HTML файла
+    }
+}
---
+++ garbandgo/src/test/java/com/example/garbandgo/GarbandgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.garbandgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbandgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
