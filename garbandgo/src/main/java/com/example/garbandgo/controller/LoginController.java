79a25cf2dc2a0a883fe298cefa149e64535fc1c9
Mila Yonkova <48622362+MilaDimitrova@users.noreply.github.com>
2025-04-17 11:58:00 +0300

    editing logIn controller, service and view

--- garbandgo/src/main/java/com/example/garbandgo/controller/LoginController.java
+++ garbandgo/src/main/java/com/example/garbandgo/controller/LoginController.java
@@ -1,5 +1,19 @@
 package com.example.garbandgo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
 public class LoginController {
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "users/login"; // Търси login.html в templates/users/
+    }
+
+    @PostMapping("/login")
+    public String processLogin() {
+        return "redirect:/home";
+    }
+
 }
