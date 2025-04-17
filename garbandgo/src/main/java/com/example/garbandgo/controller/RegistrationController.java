cfc28af648b57c3236917cc2331ad43ade359df7
Mila Yonkova <48622362+MilaDimitrova@users.noreply.github.com>
2025-04-17 12:31:44 +0300

    editing registration controller, service and view

--- garbandgo/src/main/java/com/example/garbandgo/controller/RegistrationController.java
+++ garbandgo/src/main/java/com/example/garbandgo/controller/RegistrationController.java
@@ -1,14 +1,29 @@
 package com.example.garbandgo.controller;
 
-import com.example.garbandgo.repositories.UserRepository;
+import com.example.garbandgo.entities.User;
+import com.example.garbandgo.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
+@RequestMapping("/register")
 public class RegistrationController {
-    private final UserRepository userRepository;
-    public RegistrationController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    private final UserService userService;
+
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
     }
 
+    @GetMapping
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "users/register";
+    }
 
-
-
+    @PostMapping
+    public String processRegistration(@ModelAttribute User user) {
+        userService.registerUser(user);
+        return "redirect:/login";
+    }
 }
