35b0eced091788bd400e914362227d2b27587702
Mila Yonkova <48622362+MilaDimitrova@users.noreply.github.com>
2025-04-17 11:58:00 +0300

    editing logIn controller, service and view

--- garbandgo/src/main/java/com/example/garbandgo/service/UserService.java
+++ garbandgo/src/main/java/com/example/garbandgo/service/UserService.java
@@ -4,20 +4,31 @@ import com.example.garbandgo.entities.User;
 import com.example.garbandgo.repositories.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Optional;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-
     public User registerUser(User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new RuntimeException("Имейлът вече се използва!");
+        }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        logger.info("Регистриране на нов потребител: " + user.getEmail());
         return userRepository.save(user);
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(Math.toIntExact(id));
     }
 }
