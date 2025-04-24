133ddb00f7bfd4e3e2f8289219a5382784a3550a
Mila Yonkova <48622362+MilaDimitrova@users.noreply.github.com>
2025-04-24 22:43:44 +0300

    adding new feature about rest owner

--- garbandgo/src/main/java/com/example/garbandgo/repositories/UserRepository.java
+++ garbandgo/src/main/java/com/example/garbandgo/repositories/UserRepository.java
@@ -2,10 +2,15 @@ package com.example.garbandgo.repositories;
 
 import com.example.garbandgo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
+    @Query("SELECT u FROM User u WHERE u.role.role = :role")
+    List<User> findAllByRoleName(@Param("role") String role);
 }
